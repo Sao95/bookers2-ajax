88ba85617e554094e3b752f08e59c415b8ca97ca
EC2 Default User <ec2-user@ip-172-31-40-197.ec2.internal>
2021-12-01 14:47:14 +0000

    [Finish] bookers2-debug

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,24 @@
+class ApplicationController < ActionController::Base
+
+  before_action :authenticate_user!,except: [:top, :about]
+	before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+  def after_sign_in_path_for(resource)
+    user_path(resource)
+  end
+
+  def after_sign_out_path_for(resource)
+    root_path
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
+    
+  end
+  
+  private
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+end
